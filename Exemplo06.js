456514650c3d9724dc4b1c55d12246e504545cb9
Gustavo Gustzaki <Gustavo_Gustzaki@estudante.sesisenai.org.br>
2026-09-03 21:17:15 -0300

    Deleta oque está errado

--- Exemplo06.js
+++
@@ -1,5 +0,0 @@
-const idade = 16;
-const temCnh = true;
-const podeDirigir = idade >= 18 && temCnh;
-// resultado: true
-console.log(podeDirigir)
--- Exemplo09.js
+++
@@ -1,4 +0,0 @@
-const idade = 20;
-const status = idade >= 18 ? 'Maior de idade' : 'Menor de idade';
-
-console.log(status); // Imprime: 'Maior de idade'
